30752954d3c5b7b9f3a0d1ed985a4ecc619bf91c
swapnamet <131564707+swapnamet@users.noreply.github.com>
2023-04-25 18:55:03 -0700

    Add files via upload
    comparing 2 numbers

---
+++ Compare.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Compare {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 60;
+        b = 40;
+        if (a > b) {
+            System.out.println("a is greater");
+        } else {
+            System.out.println("b is greater");
+        }
+    }
+
+    // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+    //for (int i = 1; i <= 5; i++) {
+
+    // Press Shift+F9 to start debugging your code. We have set one breakpoint
+    // for you, but you can always add more by pressing Ctrl+F8.
+    //System.out.println("i = " + i);
+    //}
+    //}
+}
